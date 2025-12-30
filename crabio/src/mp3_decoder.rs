0e2def420147823ee5795307cf14d6be2388c0ab
Dawid Królak <taavit@gmail.com>
2025-12-30 18:53:06 +0100

    ftdc working

--- crabio/src/mp3_decoder.rs
+++ crabio/src/mp3_decoder.rs
@@ -753,62 +753,279 @@ pub fn polyphase_mono(mut pcm: &mut [i16], vbuf: &[i32], coef_base: &[u32]) {
  *              guard bit analysis verified by exhaustive testing of all 2^32
  *                combinations of max pos/max neg values in x[]
  **********************************************************************************************************************/
- 
-const m_COS0_0: i32 = 0x4013c251;  /* Q31 */
-const m_COS0_1: i32 = 0x40b345bd;  /* Q31 */
-const m_COS0_2: i32 = 0x41fa2d6d;  /* Q31 */
-const m_COS0_3: i32 = 0x43f93421;  /* Q31 */
-const m_COS0_4: i32 = 0x46cc1bc4;  /* Q31 */
-const m_COS0_5: i32 = 0x4a9d9cf0;  /* Q31 */
-const m_COS0_6: i32 = 0x4fae3711;  /* Q31 */
-const m_COS0_7: i32 = 0x56601ea7;  /* Q31 */
-const m_COS0_8: i32 = 0x5f4cf6eb;  /* Q31 */
-const m_COS0_9: i32 = 0x6b6fcf26;  /* Q31 */
-const m_COS0_10: i32= 0x7c7d1db3;  /* Q31 */
-const m_COS0_11: i32= 0x4ad81a97;  /* Q30 */
-const m_COS0_12: i32= 0x5efc8d96;  /* Q30 */
-const m_COS0_13: i32= 0x41d95790;  /* Q29 */
-const m_COS0_14: i32= 0x6d0b20cf;  /* Q29 */
-const m_COS0_15: i32= 0x518522fb;  /* Q27 */
-const m_COS1_0: i32 = 0x404f4672;  /* Q31 */
-const m_COS1_1: i32 = 0x42e13c10;  /* Q31 */
-const m_COS1_2: i32 = 0x48919f44;  /* Q31 */
-const m_COS1_3: i32 = 0x52cb0e63;  /* Q31 */
-const m_COS1_4: i32 = 0x64e2402e;  /* Q31 */
-const m_COS1_5: i32 = 0x43e224a9;  /* Q30 */
-const m_COS1_6: i32 = 0x6e3c92c1;  /* Q30 */
-const m_COS1_7: i32 = 0x519e4e04;  /* Q28 */
-const m_COS2_0: i32 = 0x4140fb46;  /* Q31 */
-const m_COS2_1: i32 = 0x4cf8de88;  /* Q31 */
-const m_COS2_2: i32 = 0x73326bbf;  /* Q31 */
-const m_COS2_3: i32 = 0x52036742;  /* Q29 */
-const m_COS3_0: i32 = 0x4545e9ef;  /* Q31 */
-const m_COS3_1: i32 = 0x539eba45;  /* Q30 */
-const m_COS4_0: i32 = 0x5a82799a;  /* Q31 */
+const FDCT32S1S2: [i32; 16] = [5,3,3,2,2,1,1,1, 1,1,1,1,1,2,2,4];
 
-const m_dcttab:[i32; 48] = [ // faster in ROM
+const M_COS0_0: i32 = 0x4013c251;  /* Q31 */
+const M_COS0_1: i32 = 0x40b345bd;  /* Q31 */
+const M_COS0_2: i32 = 0x41fa2d6d;  /* Q31 */
+const M_COS0_3: i32 = 0x43f93421;  /* Q31 */
+const M_COS0_4: i32 = 0x46cc1bc4;  /* Q31 */
+const M_COS0_5: i32 = 0x4a9d9cf0;  /* Q31 */
+const M_COS0_6: i32 = 0x4fae3711;  /* Q31 */
+const M_COS0_7: i32 = 0x56601ea7;  /* Q31 */
+const M_COS0_8: i32 = 0x5f4cf6eb;  /* Q31 */
+const M_COS0_9: i32 = 0x6b6fcf26;  /* Q31 */
+const M_COS0_10: i32= 0x7c7d1db3;  /* Q31 */
+const M_COS0_11: i32= 0x4ad81a97;  /* Q30 */
+const M_COS0_12: i32= 0x5efc8d96;  /* Q30 */
+const M_COS0_13: i32= 0x41d95790;  /* Q29 */
+const M_COS0_14: i32= 0x6d0b20cf;  /* Q29 */
+const M_COS0_15: i32= 0x518522fb;  /* Q27 */
+const M_COS1_0: i32 = 0x404f4672;  /* Q31 */
+const M_COS1_1: i32 = 0x42e13c10;  /* Q31 */
+const M_COS1_2: i32 = 0x48919f44;  /* Q31 */
+const M_COS1_3: i32 = 0x52cb0e63;  /* Q31 */
+const M_COS1_4: i32 = 0x64e2402e;  /* Q31 */
+const M_COS1_5: i32 = 0x43e224a9;  /* Q30 */
+const M_COS1_6: i32 = 0x6e3c92c1;  /* Q30 */
+const M_COS1_7: i32 = 0x519e4e04;  /* Q28 */
+const M_COS2_0: i32 = 0x4140fb46;  /* Q31 */
+const M_COS2_1: i32 = 0x4cf8de88;  /* Q31 */
+const M_COS2_2: i32 = 0x73326bbf;  /* Q31 */
+const M_COS2_3: i32 = 0x52036742;  /* Q29 */
+const M_COS3_0: i32 = 0x4545e9ef;  /* Q31 */
+const M_COS3_1: i32 = 0x539eba45;  /* Q30 */
+const M_COS4_0: i32 = 0x5a82799a;  /* Q31 */
+
+const M_DCTTAB: [i32; 48] = [
     /* first pass */
-     m_COS0_0,  m_COS0_15, m_COS1_0,    /* 31, 27, 31 */
-     m_COS0_1,  m_COS0_14, m_COS1_1,    /* 31, 29, 31 */
-     m_COS0_2,  m_COS0_13, m_COS1_2,    /* 31, 29, 31 */
-     m_COS0_3,  m_COS0_12, m_COS1_3,    /* 31, 30, 31 */
-     m_COS0_4,  m_COS0_11, m_COS1_4,    /* 31, 30, 31 */
-     m_COS0_5,  m_COS0_10, m_COS1_5,    /* 31, 31, 30 */
-     m_COS0_6,  m_COS0_9,  m_COS1_6,    /* 31, 31, 30 */
-     m_COS0_7,  m_COS0_8,  m_COS1_7,    /* 31, 31, 28 */
+     M_COS0_0,  M_COS0_15, M_COS1_0,    /* 31, 27, 31 */
+     M_COS0_1,  M_COS0_14, M_COS1_1,    /* 31, 29, 31 */
+     M_COS0_2,  M_COS0_13, M_COS1_2,    /* 31, 29, 31 */
+     M_COS0_3,  M_COS0_12, M_COS1_3,    /* 31, 30, 31 */
+     M_COS0_4,  M_COS0_11, M_COS1_4,    /* 31, 30, 31 */
+     M_COS0_5,  M_COS0_10, M_COS1_5,    /* 31, 31, 30 */
+     M_COS0_6,  M_COS0_9,  M_COS1_6,    /* 31, 31, 30 */
+     M_COS0_7,  M_COS0_8,  M_COS1_7,    /* 31, 31, 28 */
     /* second pass */
-     m_COS2_0,  m_COS2_3,  m_COS3_0,   /* 31, 29, 31 */
-     m_COS2_1,  m_COS2_2,  m_COS3_1,   /* 31, 31, 30 */
-    -m_COS2_0, -m_COS2_3,  m_COS3_0,   /* 31, 29, 31 */
-    -m_COS2_1, -m_COS2_2,  m_COS3_1,   /* 31, 31, 30 */
-     m_COS2_0,  m_COS2_3,  m_COS3_0,   /* 31, 29, 31 */
-     m_COS2_1,  m_COS2_2,  m_COS3_1,   /* 31, 31, 30 */
-    -m_COS2_0, -m_COS2_3,  m_COS3_0,   /* 31, 29, 31 */
-    -m_COS2_1, -m_COS2_2,  m_COS3_1,   /* 31, 31, 30 */
+     M_COS2_0,  M_COS2_3,  M_COS3_0,   /* 31, 29, 31 */
+     M_COS2_1,  M_COS2_2,  M_COS3_1,   /* 31, 31, 30 */
+    -M_COS2_0, -M_COS2_3,  M_COS3_0,   /* 31, 29, 31 */
+    -M_COS2_1, -M_COS2_2,  M_COS3_1,   /* 31, 31, 30 */
+     M_COS2_0,  M_COS2_3,  M_COS3_0,   /* 31, 29, 31 */
+     M_COS2_1,  M_COS2_2,  M_COS3_1,   /* 31, 31, 30 */
+    -M_COS2_0, -M_COS2_3,  M_COS3_0,   /* 31, 29, 31 */
+    -M_COS2_1, -M_COS2_2,  M_COS3_1,   /* 31, 31, 30 */
 ];
 
-#[allow(non_upper_case_globals)]
-const FDCT32s1s2: [u8; 16] = [5,3,3,2,2,1,1,1, 1,1,1,1,1,2,2,4];
+
+pub fn fdct_32(buf_slice: &mut[i32], dest_slice: &mut[i32], offset: i32, odd_block: i32, gb: i32) {
+    let mut es = 0;
+    if gb < 6 {
+        es = 6 - gb;
+        for d in buf_slice.iter_mut() {
+            *d >>= es;
+        }
+    }
+
+    /* first pass */
+    for i in 0..8 {
+        let a0 = buf_slice[i];
+        let a1 = buf_slice[15 - i];
+        let a2 = buf_slice[16 + i];
+        let a3 = buf_slice[31 - i];
+
+        let b0 = a0 + a3;
+        let b3 = mulshift_32(M_DCTTAB[i * 3], a0 - a3) << 1;
+
+        let b1 = a1 + a2;
+        let b2 = mulshift_32(M_DCTTAB[i * 3 + 1], a1 - a2) << FDCT32S1S2[i] as i32;
+
+        let coeff = M_DCTTAB[i * 3 + 2]; // shared for next two uses
+        buf_slice[i] = b0 + b1;
+        buf_slice[15 - i] = mulshift_32(coeff, b0 - b1) << FDCT32S1S2[8 + i] as i32;
+
+        buf_slice[16 + i] = b2 + b3;
+        buf_slice[31 - i] = mulshift_32(coeff, b3 - b2) << FDCT32S1S2[8 + i] as i32;
+    }
+
+    let cptr_slice_second = &M_DCTTAB[24..];
+    /* second pass */
+    for (idx, buf_chunk) in buf_slice.chunks_exact_mut(8).enumerate() {
+        let a0 = buf_chunk[0];
+        let a3 = buf_chunk[3];
+        let a4 = buf_chunk[4];
+        let a7 = buf_chunk[7];
+
+        let b0 = a0 + a7;
+        let b7 = mulshift_32(cptr_slice_second[idx * 6], a0 - a7) << 1;
+
+        let b3 = a3 + a4;
+        let b4 = mulshift_32(cptr_slice_second[idx * 6 + 1], a3 - a4) << 3;
+
+
+        let t0 = b0 + b3;
+        let t3 = mulshift_32(cptr_slice_second[idx * 6 + 2], b0 - b3) << 1;
+
+        let t4 = b4 + b7;
+        let t7 = mulshift_32(cptr_slice_second[idx * 6 + 2], b7 - b4) << 1;
+
+
+        let a1 = buf_chunk[1];
+        let a6 = buf_chunk[6];
+        let a2 = buf_chunk[2];
+        let a5 = buf_chunk[5];
+
+        let b1 = a1 + a6;
+        let b6 = mulshift_32(cptr_slice_second[idx * 6 + 3], a1 - a6) << 1;
+
+        let b2 = a2 + a5;
+        let b5 = mulshift_32(cptr_slice_second[idx * 6 + 4], a2 - a5) << 1;
+
+        let t1 = b1 + b2;
+        let t2 = mulshift_32(cptr_slice_second[idx * 6 + 5], b1 - b2) << 2;
+
+        let t5 = b5 + b6;
+        let t6 = mulshift_32(cptr_slice_second[idx * 6 + 5], b6 - b5) << 2;
+
+        let bb0 = t0 + t1;
+        let bb1 = mulshift_32(M_COS4_0, t0 - t1) << 1;
+        let bb2 = t2 + t3;
+        let bb3 = mulshift_32(M_COS4_0, t3 - t2) << 1;
+
+        buf_chunk[0] = bb0;
+        buf_chunk[1] = bb1;
+        buf_chunk[2] = bb2 + bb3;
+        buf_chunk[3] = bb3;
+
+        let bb4 = t4 + t5;
+        let bb5 = mulshift_32(M_COS4_0, t4 - t5) << 1;
+        let bb6 = t6 + t7;
+        let bb7 = mulshift_32(M_COS4_0, t7 - t6) << 1;
+        let bb6_sum = bb6 + bb7;
+
+        buf_chunk[4] = bb4 + bb6_sum;
+        buf_chunk[5] = bb5 + bb7;
+        buf_chunk[6] = bb5 + bb6_sum;
+        buf_chunk[7] = bb7;
+    }
+
+    /* sample 0 - always delayed one block */
+    let mut d = &mut dest_slice[64 * 16 + ((offset - odd_block) & 7) as usize + if odd_block != 0 { 0 } else { VBUF_LENGTH }..];
+    let s = buf_slice[0];
+    d[0] = s;
+    d[8] = s;
+
+    /* samples 16 to 31 */
+    d = &mut dest_slice[offset as usize + if odd_block != 0 { VBUF_LENGTH } else { 0 }..];
+
+    let mut s = buf_slice[1];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    let mut tmp = buf_slice[25] + buf_slice[29];
+    s = buf_slice[17] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[9] + buf_slice[13];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[21] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[29] + buf_slice[27];
+    s = buf_slice[5];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[21] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[13] + buf_slice[11];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[19] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[27] + buf_slice[31];
+    s = buf_slice[3];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[19] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[11] + buf_slice[15];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[23] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[31];
+    s = buf_slice[7];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[23] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[15];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = tmp;
+    d[0] = s; d[8] = s;
+
+    /* samples 1 to 16 */
+    d = &mut dest_slice[16 + ((offset - odd_block) & 7) as usize + if odd_block != 0 { 0 } else { VBUF_LENGTH }..];
+
+    s = buf_slice[1];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[30] + buf_slice[25];
+    s = buf_slice[17] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[14] + buf_slice[9];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[22] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[6];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[26] + buf_slice[30];
+    s = buf_slice[22] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[10] + buf_slice[14];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[18] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[2];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[28] + buf_slice[26];
+    s = buf_slice[18] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[12] + buf_slice[10];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[20] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[4];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+
+    tmp = buf_slice[24] + buf_slice[28];
+    s = buf_slice[20] + tmp;
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[8] + buf_slice[12];
+    d[0] = s; d[8] = s; d = &mut d[64..];
+    s = buf_slice[16] + tmp;
+    d[0] = s; d[8] = s;
+
+    /* final rescale + clip if es > 0 (rare) */
+    if es != 0 {
+        let n_clip = (31 - es) as u32;
+
+        d = &mut dest_slice[64 * 16 + ((offset - odd_block) & 7) as usize + if odd_block != 0 { 0 } else { VBUF_LENGTH }..];
+        s = d[0];
+        s = clip_2n(s, n_clip);
+        s <<= es;
+        d[0] = s;
+        d[8] = s;
+
+        d = &mut dest_slice[offset as usize + if odd_block != 0 { VBUF_LENGTH } else { 0 }..];
+        for _ in 16..=31 {
+            s = d[0];
+            s = clip_2n(s, n_clip);
+            s <<= es;
+            d[0] = s;
+            d[8] = s;
+            d = &mut d[64..];
+        }
+
+        d = &mut dest_slice[16 + ((offset - odd_block) & 7) as usize + if odd_block != 0 { 0 } else { VBUF_LENGTH }..];
+        for _ in 0..16 {
+            s = d[0];
+            s = clip_2n(s, n_clip);
+            s <<= es;
+            d[0] = s;
+            d[8] = s;
+            d = &mut d[64..];
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
